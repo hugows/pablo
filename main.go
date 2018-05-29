11de959804b3bb4261b834a9333fd25818e115c8
Hugo Schmitt <hugows@unisinos.br>
2018-05-29 12:40:14 -0300

    the proper thing here is to move logic to the frontend

--- main.go
+++ main.go
@@ -1,68 +1,31 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo"
+	"github.com/pkg/browser"
 	"gopkg.in/olahol/melody.v1"
 )
 
-// Stream represents a data source - each unique URI posted by the user becomes a stream.
-// The user may choose to hide any streams (accidental POST).
-type Stream struct {
-	path   string
-	varMap map[string]string
-	hidden bool
-}
-
-// Chart are the charts being displayed on the current page.
-// The user creates those charts after seeing the streams pop-up on the page.
-type Chart struct {
-	// Stream (currently the "unique URL") to use as a data source for this chart
-	stream *Stream
-
-	// Extract from JSON to plot on the Y axis
-	keyY string
-
-	// Extract from JSON to plot on the X axis
-	keyX string
-
-	// Instead of getting an X value from the JSON struct, the user can also use the time
-	// when the POST was received
-	UseServerTimeForX bool
-}
+const serverAddr = ":5000"
 
 // Server struct to share required data for HTTP handlers
 type Server struct {
-	ws      *melody.Melody
-	router  *echo.Echo
-	streams map[string]*Stream
-	charts  []*Chart
+	ws     *melody.Melody
+	router *echo.Echo
 }
 
 func main() {
 	s := Server{
-		router:  echo.New(),
-		ws:      melody.New(),
-		streams: make(map[string]*Stream),
-		charts:  make([]*Chart, 0),
+		router: echo.New(),
+		ws:     melody.New(),
 	}
 
-	// DEBUG
+	s.setup()
 
-	s.streams["/api/update"] = &Stream{
-		path: "/api/update",
-	}
+	url := fmt.Sprintf("http://localhost%s", serverAddr)
+	browser.OpenURL(url)
 
-	s.charts = append(s.charts, &Chart{
-		stream: s.streams["/api/update"],
-		keyX:   "temp",
-		keyY:   "press",
-	})
-
-	s.middlewares()
-	s.routes()
-
-	// url := fmt.Sprintf("http://localhost%s", addr)
-	// browser.OpenURL(url)
-
-	s.start(":5000", false)
+	s.start(serverAddr)
 }
--- routes.go
+++ routes.go
@@ -1,39 +1,75 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/gobuffalo/packr"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	melody "gopkg.in/olahol/melody.v1"
 )
 
-func (s *Server) middlewares() {
+func (s *Server) setup() {
+	// Middlewares
 	s.router.Pre(middleware.RemoveTrailingSlash())
-	// s.router.Use(middleware.Logger())
 	s.router.Use(middleware.Recover())
-}
 
-func (s *Server) routes() {
+	// Packr is used so we can deliver a single binary to the end user
 	assetHandler := http.FileServer(packr.NewBox("./static"))
 
+	// Serve index.html
 	s.router.GET("/", echo.WrapHandler(assetHandler))
+
+	// Serve js/css/...
 	s.router.GET("/___INTERNAL/static/*", echo.WrapHandler(http.StripPrefix("/___INTERNAL/static/", assetHandler)))
+
+	// Start a websocket connection
 	s.router.GET("/___INTERNAL/ws", func(c echo.Context) error {
 		s.ws.HandleRequest(c.Response(), c.Request())
 		return nil
 	})
 
-	s.ws.HandleMessage(func(sess *melody.Session, msg []byte) {
-		s.ws.Broadcast(msg)
-	})
+	// Receives POST data, filter numeric fields, adds special fields
+	// (server timestamp, original URI) and broadcasts to the connected
+	// client on the browser.
+	s.router.POST("/*", func(c echo.Context) error {
+		uri := c.Request().RequestURI
 
-	s.router.POST("/*", s.handleUserPost)
+		body, _ := ioutil.ReadAll(c.Request().Body)
+		var input interface{}
+
+		err := json.Unmarshal(body, &input)
+		if err != nil {
+			return err
+		}
+
+		minput := input.(map[string]interface{})
+		output := make(map[string]interface{})
+
+		for k, v := range minput {
+			switch vv := v.(type) {
+			case float64:
+				output[k] = vv
+			}
+		}
+
+		output["_timestamp"] = time.Now()
+		output["_uri"] = uri
+
+		msg, err := json.Marshal(output)
+
+		if err != nil {
+			return err
+		}
+
+		s.ws.Broadcast(msg)
+		return nil
+	})
 }
 
-func (s *Server) start(addr string, silent bool) {
-	s.router.HideBanner = silent
-	s.router.HidePort = silent
+func (s *Server) start(addr string) {
+	s.router.HideBanner = true
 	s.router.Logger.Fatal(s.router.Start(addr))
 }
